6ec8da8cadb673848a8b99d59f5e20179c3fa07b
toshinori.sato <toshinori.sato@goodpatch-s.com>
2023-10-13 11:46:12 +0900

    ハートのアニメーション完成？

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import './style.css'
 
-const btn = document.getElementById('btn');
-const overlay = document.querySelector('.overlay');
+const btn0 = document.getElementById('btn0');
+const btn50 = document.getElementById('btn50');
+const btn100 = document.getElementById('btn100');
+const progress = document.querySelector('.progress');
 
-if (btn && overlay) {
-  let isSet = false;
-  btn.addEventListener("click", () => {
-    if (isSet) {
-      overlay.classList.remove('show');
-    } else {
-      overlay.classList.add('show');
-    }
-
-    isSet = !isSet;
+if (btn0 && btn50 && btn100 && progress) {
+  btn0.addEventListener("click", () => {
+    progress.setAttribute("style", "height: 16px;");
+  })
+  btn50.addEventListener("click", () => {
+    progress.setAttribute("style", "height: 8px;");
+  })
+  btn100.addEventListener("click", () => {
+    progress.setAttribute("style", "height: 0px;");
   })
 }
