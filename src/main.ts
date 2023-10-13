aea826b80cc1b38824a63d14e413f16569c10286
toshinori.sato <toshinori.sato@goodpatch-s.com>
2023-10-13 11:10:47 +0900

    ハートのSVG作成

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import './style.css'
+
+const btn = document.getElementById('btn');
+const overlay = document.querySelector('.overlay');
+
+if (btn && overlay) {
+  let isSet = false;
+  btn.addEventListener("click", () => {
+    if (isSet) {
+      overlay.classList.remove('show');
+    } else {
+      overlay.classList.add('show');
+    }
+
+    isSet = !isSet;
+  })
+}
